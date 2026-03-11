3caa4c2ef934a277eea31da3f2b0b536780368b6
Eduardo Gonçalves <eduardouc388@gmail.com>
2026-03-10 23:46:35 -0300

    Update

--- src/main/java/com/eduuardo/forumhub/ForumhubApplication.java
+++ src/main/java/com/eduuardo/forumhub/ForumhubApplication.java
@@ -2,7 +2,6 @@ package com.eduuardo.forumhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class ForumhubApplication {
